88ea621133271e630a4d878eb757782405eb7b56
Tony.J <kavinjsir@gmail.com>
2022-08-19 00:59:24 -0400

    feat: add profile image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,9 @@
-import { Container, Box, Heading } from '@chakra-ui/react';
+import { Container, Box, Heading, chakra } from '@chakra-ui/react';
+import Image from 'next/image';
+
+const ProfileImage = chakra(Image, {
+  shouldForwardProp: (prop) => [`width`, `height`, `src`, `alt`].includes(prop),
+});
 
 const Page = () => {
   return (
@@ -8,13 +13,38 @@ const Page = () => {
       </Box>
 
       <Box display="flex">
-        <Box /*flexGrow={1}*/>
+        <Box flexGrow={1}>
           <Heading as="h2" variant="page-title">
             Tony Gu
           </Heading>
+          <p>TODO: lalalalala</p>
+        </Box>
+        <Box
+          flexShrink={0}
+          mt={{ base: 4, md: 0 }}
+          ml={{ md: 6 }}
+          textAlign="center"
+        >
+          <Box
+            borderColor="whiteAlpha.800"
+            borderWidth={2}
+            borderStyle="solid"
+            w="100px"
+            h="100px"
+            display="inline-block"
+            borderRadius="full"
+            overflow="hidden"
+          >
+            <ProfileImage
+              src="/images/profile.jpeg"
+              alt="Profile image"
+              borderRadius="full"
+              width="100%"
+              height="100%"
+            />
+          </Box>
         </Box>
       </Box>
-      <p>TODO: shuoshuo ni zi ji</p>
     </Container>
   );
 };
