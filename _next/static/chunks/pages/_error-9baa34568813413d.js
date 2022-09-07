e6b111cf0aa6eb019b86426b17b1fe1444d60e1a
Kavinjsir <kavinjsir@gmail.com>
2022-09-07 08:46:08 +0000

    Deploying to public from @ Kavinjsir/site@81b93a3896aaf5dfb347ad3a45ece8c1cfa79a71 🚀

---
+++ _next/static/chunks/pages/_error-9baa34568813413d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[820],{9396:function(a,b,c){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return c(4317)}])}},function(a){a.O(0,[774,888,179],function(){var b;return a(a.s=9396)}),_N_E=a.O()}])
